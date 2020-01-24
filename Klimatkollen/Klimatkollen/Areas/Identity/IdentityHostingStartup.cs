3a9a0aed5e50922ba844753c8373ca43038b1f0d
Benjamin Ek <benjaminek@live.se>
2020-01-24 12:30:22 +0100

    Lagt till möjlighet att skapa användarroller, samt scaffoldat lite användarmiljö

---
+++ Klimatkollen/Klimatkollen/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Klimatkollen.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+//[assembly: HostingStartup(typeof(Klimatkollen.Areas.Identity.IdentityHostingStartup))]
+//namespace Klimatkollen.Areas.Identity
+//{
+//    public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) => {
+//                services.AddDbContext<KlimatkollenContext>(options =>
+//                    options.UseSqlServer(
+//                        context.Configuration.GetConnectionString("KlimatkollenContextConnection")));
+
+//                services.AddDefaultIdentity<IdentityUser>()
+//                    .AddEntityFrameworkStores<KlimatkollenContext>();
+//            });
+//        }
+//    }
+//}
