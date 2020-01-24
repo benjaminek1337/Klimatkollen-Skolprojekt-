6b0d0f79b59e9684f0df9ef39a9f47916f230057
Benjamin Ek <benjaminek@live.se>
2020-01-24 13:59:12 +0100

    Kan ändra och ta bort olika systemroller

--- Klimatkollen/Klimatkollen/Controllers/AdminController.cs
+++ Klimatkollen/Klimatkollen/Controllers/AdminController.cs
@@ -11,10 +11,12 @@ namespace Klimatkollen.Controllers
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
 
@@ -37,7 +39,7 @@ namespace Klimatkollen.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("index", "home");
+                    return RedirectToAction("listroles", "admin");
                 }
 
                 foreach(IdentityError error in result.Errors)
@@ -47,6 +49,90 @@ namespace Klimatkollen.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Systemroll ID: {id} kan inte hittas.";
+                return View("ListRoles");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+                
+            };
+
+            foreach(var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Systemroll ID: {model.Id} kan inte hittas.";
+                return View("ListRoles");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Systemroll ID: {id} kan inte hittas.";
+                return View("ListRoles");
+            }
+            else
+            {
+                await roleManager.DeleteAsync(role);
+                var result = await roleManager.UpdateAsync(role);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+
+                }
+            }
+
+            return RedirectToAction("ListRoles");
         }
 
     }
