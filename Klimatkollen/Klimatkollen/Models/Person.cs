687924ade0c7152cfe3c39e70c24f9175b0f1609
Johan Eriksson <joer1500@student.miun.se>
2020-01-27 13:59:14 +0100

    Uppdaterat klassen för person och skapat controller

--- Klimatkollen/Klimatkollen/Models/Person.cs
+++ Klimatkollen/Klimatkollen/Models/Person.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Klimatkollen.Models
 {
     public class Person
     {
-        public String UserName { get; set; }
+        [Key]
+        [Required(ErrorMessage = "Vänligen fyll i en korrekt e-postadress")]
+        [Display(Name = "E-postadress")]
+        [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
+        [Required(ErrorMessage = "Vänligen fyll i användarnamn")]
+        [Display(Name = "Användarnamn")]
+        public String UserName { get; set; }
+        [Display(Name = "Förnamn")]
+        public String FirstName { get; set; }
+        [Display(Name = "Efternamn")]
+        public String Lastname { get; set; }
 
 
-
-        
     }
 }
